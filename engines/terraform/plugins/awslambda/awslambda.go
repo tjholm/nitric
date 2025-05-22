ed50025b49fe3c24d8cbc6ac6d717da4d8f92bdf
Tim Holm <timothy.j.holm@gmail.com>
2025-05-22 10:44:09 +1000

    remove broken code.

--- engines/terraform/plugins/awslambda/awslambda.go
+++ engines/terraform/plugins/awslambda/awslambda.go
@@ -1,18 +1,1 @@
 package awslambda
-
-import (
-	"github.com/nitrictech/nitric/engines/terraform/plugins/service"
-)
-
-type AwsLambdaService struct {
-}
-
-var _ service.Service = (*AwsLambdaService)(nil)
-
-func (s *AwsLambdaService) Start(proxy service.Proxy) error {
-	return nil
-}
-
-func New() *AwsLambdaService {
-	return &AwsLambdaService{}
-}
